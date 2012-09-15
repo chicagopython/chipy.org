010fddb17417c8a02b0678da3cd9a4c467e7968c
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-09-14 23:08:11 -0500

    Put SITE_ID into env

--- chipy_org/settings.py
+++ chipy_org/settings.py
@@ -45,7 +45,7 @@ LOGIN_REDIRECT_URL = '/'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "en-us"
 
-SITE_ID = 1
+SITE_ID = env.get('SITE_ID', 1)
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.

eca267c1187a4748dc0c6cd02480e4c8b4e5beb6
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-10-13 15:54:30 -0500

    User required for my talks

--- chipy_org/apps/meetings/models.py
+++ chipy_org/apps/meetings/models.py
@@ -75,6 +75,7 @@ class Presentor(CommonModel):
     def __unicode__(self):
         return self.name
 
+    user = models.ForeignKey(User, blank = True, null = True)
     name = models.CharField(max_length = MAX_LENGTH)
     email = models.EmailField(max_length = MAX_LENGTH, blank = True, null = True)
     phone = models.CharField(max_length = MAX_LENGTH, blank = True, null = True)

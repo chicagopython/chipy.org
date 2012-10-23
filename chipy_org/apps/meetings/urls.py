92ef7279d66c96be8a1cbbacb82201a5ca39a9f8
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-10-22 22:14:08 -0500

    Added rsvp url

--- chipy_org/apps/meetings/urls.py
+++ chipy_org/apps/meetings/urls.py
@@ -3,10 +3,12 @@ from django.contrib.auth.decorators import login_required
 from meetings.views import (PastMeetings,
                             ProposeTopic,
                             MyTopics,
+                            RSVP,
 )
 
 urlpatterns = patterns("",
     url(r'^past/$', PastMeetings.as_view(), name='past_meetings'),
+    url(r'^rsvp/$', RSVP.as_view(), name='rsvp'),
     url(r'^topics/propose$', login_required(ProposeTopic.as_view()), name='propose_topic'),
     url(r'^topics/mine$', login_required(MyTopics.as_view()), name='my_topics'),
 )

92f2aca2929b950a032ec190f357684498229c7a
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-11-30 22:31:14 -0600

    Only showing opt-in profiles

--- chipy_org/apps/profiles/views.py
+++ chipy_org/apps/profiles/views.py
@@ -6,10 +6,10 @@ from profiles.models import UserProfile
 from profiles.forms import ProfileForm
 
 class ProfilesList(ListView):
-    model = User
     context_object_name = 'profiles'
     template_name = 'profiles/list.html'
-
+    queryset = User.objects.filter(profiles__show = True)
+    
 class ProfileEdit(UpdateView):
     form_class = ProfileForm
     template_name = "profiles/edit.html"

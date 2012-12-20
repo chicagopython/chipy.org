23eda08ca7689ca9f914144b6920b28ae9ee46e9
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-12-19 21:47:32 -0600

    Linked up the about page

--- chipy_org/apps/about/urls.py
+++ chipy_org/apps/about/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "about/about.html"}, name="about"),
+    url(r"^$", 'django.contrib.flatpages.views.flatpage', {'url': '/about/'}, name='about'),
     url(r"^terms/$", direct_to_template, {"template": "about/terms.html"}, name="terms"),
     url(r"^privacy/$", direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
     url(r"^dmca/$", direct_to_template, {"template": "about/dmca.html"}, name="dmca"),

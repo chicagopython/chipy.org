90b9ddb7bb8f0f3fc7b091fd56900204fe5944d8
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-02-12 22:16:13 -0600

    Adding RSVP to admin

--- chipy_org/apps/meetings/admin.py
+++ chipy_org/apps/meetings/admin.py
@@ -1,8 +1,8 @@
-from models import Meeting, Venue, Topic, Presentor
+from models import Meeting, Venue, Topic, Presentor, RSVP
 from django.contrib import admin
 
 admin.site.register(Venue)
- 
+
 class MeetingAdmin(admin.ModelAdmin):
     list_display = ('when','where','created','modified')
     exclude = [
@@ -10,5 +10,4 @@ class MeetingAdmin(admin.ModelAdmin):
 admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(Topic)
 admin.site.register(Presentor)
-
-
+admin.site.register(RSVP)

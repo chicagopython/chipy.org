49dd1b5205498425f7af247f7c390a48a423db4c
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-02-20 17:52:04 -0600

    Fixes to the create_user pipeline

--- chipy_org/libs/social_auth_pipelines.py
+++ chipy_org/libs/social_auth_pipelines.py
@@ -9,14 +9,16 @@ def create_user(backend, details, response, uid, username, user = None, is_new =
     '''
     Check if a user with this email already exists. If they do, don't create an account.
     '''
-    
-    if not user:
+
+    if is_new:
         if User.objects.filter(email = details.get('email')).exists():
-            msg = ugettext('This email is already in use. First login with your other account and under the top right menu click add account.')
+            msg = ugettext('This email is already in use.')
             raise AuthAlreadyAssociated(backend, msg % {
                 'provider': backend.name
             })
-        else:
-        return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
+
+            return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
     else:
         return {}
+    
+    

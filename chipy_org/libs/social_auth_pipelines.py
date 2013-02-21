e70856cb18fa86f955dda6cb18cddbdc431a5577
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-02-20 18:09:26 -0600

    Revert "Fixes to the create_user pipeline"
    This reverts commit 49dd1b5205498425f7af247f7c390a48a423db4c.

--- chipy_org/libs/social_auth_pipelines.py
+++ chipy_org/libs/social_auth_pipelines.py
@@ -9,16 +9,14 @@ def create_user(backend, details, response, uid, username, user = None, is_new =
     '''
     Check if a user with this email already exists. If they do, don't create an account.
     '''
-
-    if is_new:
+    
+    if not user:
         if User.objects.filter(email = details.get('email')).exists():
-            msg = ugettext('This email is already in use.')
+            msg = ugettext('This email is already in use. First login with your other account and under the top right menu click add account.')
             raise AuthAlreadyAssociated(backend, msg % {
                 'provider': backend.name
             })
-
-            return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
+        else:
+        return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
     else:
         return {}
-    
-    

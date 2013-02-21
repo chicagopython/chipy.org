daeee760bc7fe1bc96c5fff35e0c4338435a7cc2
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-02-20 18:11:50 -0600

    Missing spaces

--- chipy_org/libs/social_auth_pipelines.py
+++ chipy_org/libs/social_auth_pipelines.py
@@ -9,7 +9,7 @@ def create_user(backend, details, response, uid, username, user = None, is_new =
     '''
     Check if a user with this email already exists. If they do, don't create an account.
     '''
-    
+
     if not user:
         if User.objects.filter(email = details.get('email')).exists():
             msg = ugettext('This email is already in use. First login with your other account and under the top right menu click add account.')
@@ -17,6 +17,6 @@ def create_user(backend, details, response, uid, username, user = None, is_new =
                 'provider': backend.name
             })
         else:
-        return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
+            return social_auth_create_user(backend, details, response, uid, username, user = None, *args, **kwargs)
     else:
         return {}

6441f8a8a6b22abf5c4453aca5dbedea1de130a5
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-04-28 19:06:57 -0500

    meetings.views cleanup

--- chipy_org/apps/meetings/views.py
+++ chipy_org/apps/meetings/views.py
@@ -13,9 +13,11 @@ from meetings.models import RSVP as RSVPModel
 
 from meetings.forms import TopicForm, RSVPForm
 
+
 class PastMeetings(ListView):
     template_name = 'meetings/past_meetings.html'
-    queryset = Meeting.objects.filter(when__lt = datetime.datetime.now() - datetime.timedelta(hours = 3))
+    queryset = Meeting.objects.filter(when__lt=datetime.datetime.now() - datetime.timedelta(hours=3))
+
 
 class ProposeTopic(CreateView):
     template_name = 'meetings/propose_topic.html'
@@ -24,7 +26,7 @@ class ProposeTopic(CreateView):
 
     def get_form_kwargs(self):
         kwargs = super(ProposeTopic, self).get_form_kwargs()
-        kwargs.update({'request':self.request})
+        kwargs.update({'request': self.request})
         return kwargs
 
     def post(self, request, *args, **kwargs):
@@ -39,16 +41,17 @@ class ProposeTopic(CreateView):
         else:
             return self.form_invalid(form)
 
+
 class MyTopics(ListView):
     template_name = 'meetings/my_topics.html'
 
     def get_queryset(self):
         try:
-            presentor = Presentor.objects.filter(user = self.request.user)
+            presentor = Presentor.objects.filter(user=self.request.user)
         except Presentor.DoesNotExist:
             return Topic.objects.none()
 
-        return Topic.objects.filter(presentor = presentor)
+        return Topic.objects.filter(presentor=presentor)
 
 
 class RSVP(ProcessFormView, ModelFormMixin, TemplateResponseMixin):
@@ -59,22 +62,22 @@ class RSVP(ProcessFormView, ModelFormMixin, TemplateResponseMixin):
     def get_template_names(self):
         if self.request.method == 'POST':
             return ['meetings/_rsvp_form_response.html']
-        elif  self.request.method == 'GET':
+        elif self.request.method == 'GET':
             return ['meetings/rsvp_form.html']
 
     def get_form_kwargs(self):
         kwargs = {}
         if not kwargs.get('instance', False) and self.request.user.is_authenticated() and 'rsvp_key' not in self.kwargs:
             try:
-                meeting = Meeting.objects.get(pk = self.request.POST['meeting'])
-                self.object = RSVPModel.objects.get(user = self.request.user, meeting = meeting)
+                meeting = Meeting.objects.get(pk=self.request.POST['meeting'])
+                self.object = RSVPModel.objects.get(user=self.request.user, meeting=meeting)
             except RSVPModel.DoesNotExist:
                 pass
         elif 'rsvp_key' in self.kwargs:
-            self.object = RSVPModel.objects.get(key = self.kwargs['rsvp_key'])
+            self.object = RSVPModel.objects.get(key=self.kwargs['rsvp_key'])
 
         kwargs.update(super(RSVP, self).get_form_kwargs())
-        kwargs.update({'request':self.request})
+        kwargs.update({'request': self.request})
 
         return kwargs
 
@@ -96,20 +99,22 @@ class RSVP(ProcessFormView, ModelFormMixin, TemplateResponseMixin):
         else:
             return self.form_invalid(form)
 
+
 class RSVPlist(ListView):
     context_object_name = 'attendees'
     template_name = 'meetings/rsvp_list.html'
 
     def get_queryset(self):
-        self.meeting = Meeting.objects.get(key = self.kwargs['rsvp_key'])
-        return RSVPModel.objects.filter(meeting = self.meeting).exclude(response = 'N')
+        self.meeting = Meeting.objects.get(key=self.kwargs['rsvp_key'])
+        return RSVPModel.objects.filter(meeting=self.meeting).exclude(response='N')
 
     def get_context_data(self, **kwargs):
         context = {'meeting': self.meeting}
         context.update(super(RSVPlist, self).get_context_data(**kwargs))
         return context
 
+
 class PastTopics(ListView):
     context_object_name = 'topics'
     template_name = 'meetings/past_topics.html'
-    queryset = Topic.objects.filter(meeting__when__lt = datetime.date.today(), approved = True)
+    queryset = Topic.objects.filter(meeting__when__lt=datetime.date.today(), approved=True)

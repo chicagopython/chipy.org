3fb2023c66ac9c04af9f4567dadca5c87d7f9aac
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-05-09 20:59:08 -0500

    pep8 work

--- chipy_org/apps/meetings/models.py
+++ chipy_org/apps/meetings/models.py
@@ -1,4 +1,3 @@
-import settings
 import datetime
 import string
 import random
@@ -6,7 +5,6 @@ import random
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
-from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
@@ -22,73 +20,73 @@ MEETING = (
     ('North', 'North Meeting - 3rd Thursday')
 )
 
+
 class Venue(CommonModel):
 
     def __unicode__(self):
-	return self.name
+        return self.name
 
     name = models.CharField(max_length=MAX_LENGTH)
-    email = models.EmailField(max_length=MAX_LENGTH,blank=True,null=True)
-    phone = models.CharField(max_length=MAX_LENGTH,blank=True,null=True)
-    address = models.TextField(blank=True,null=True)
+    email = models.EmailField(max_length=MAX_LENGTH, blank=True, null=True)
+    phone = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
 
     def get_latitude(self):
-        raise NotImplimented
+        raise NotImplementedError
 
     def get_longitude(self):
-        raise NotImplimented
+        raise NotImplementedError
 
     longitude = property(get_longitude)
     latitude = property(get_latitude)
-
 
     @property
     def jsonLatLng(self):
         '''
         Use the string returned as args for google.maps.LatLng constructor.
-	'''
-        if self.latitude != None and self.longitude != None:
-            return "%.6f,%.6f" % (self.latitude,self.longitude)
+        '''
+        if self.latitude is not None and self.longitude is not None:
+            return "%.6f,%.6f" % (self.latitude, self.longitude)
         else:
             return None
 
-    directions = models.TextField(blank=True,null=True)
-    embed_map = models.TextField(blank=True,null=True)
+    directions = models.TextField(blank=True, null=True)
+    embed_map = models.TextField(blank=True, null=True)
     link = models.URLField(verify_exists=True, blank=True, null=True)
-
 
 
 class Meeting(CommonModel):
     def __unicode__(self):
-	if self.where:
-	    return "%s at %s" % (self.when.strftime("%A, %B %d %Y at %I:%M %p"), self.where.name)
-	return "%s location TBD" % self.when
+        if self.where:
+            return "%s at %s" % (self.when.strftime("%A, %B %d %Y at %I:%M %p"), self.where.name)
+        return "%s location TBD" % self.when
 
     when = models.DateTimeField()
-    where = models.ForeignKey(Venue,blank=True,null=True)
-    key = models.CharField(max_length = 40, unique = True, blank = True) # Used for anonymous access to meeting information like RSVPs
+    where = models.ForeignKey(Venue, blank=True, null=True)
+    key = models.CharField(max_length=40, unique=True, blank=True)  # Used for anonymous access to meeting information like RSVPs
 
     def is_future(self):
-        return bool( self.when >=  ( datetime.datetime.now() - datetime.timedelta(hours = 3 )))
+        return bool(self.when >= (datetime.datetime.now() - datetime.timedelta(hours=3)))
 
     def rsvp_user_yes(self):
-        raise NotImplimentedError
+        raise NotImplementedError
 
     def rsvp_user_maybe(self):
-        raise NotImplimentedError
+        raise NotImplementedError
 
     def number_rsvps(self):
-        return self.rsvp_set.exclude(response = 'N').count()
+        return self.rsvp_set.exclude(response='N').count()
+
 
 class Presentor(CommonModel):
     def __unicode__(self):
         return self.name
 
-    user = models.ForeignKey(User, blank = True, null = True)
-    name = models.CharField(max_length = MAX_LENGTH)
-    email = models.EmailField(max_length = MAX_LENGTH, blank = True, null = True)
-    phone = models.CharField(max_length = MAX_LENGTH, blank = True, null = True)
-    release = models.BooleanField(default = False)
+    user = models.ForeignKey(User, blank=True, null=True)
+    name = models.CharField(max_length=MAX_LENGTH)
+    email = models.EmailField(max_length=MAX_LENGTH, blank=True, null=True)
+    phone = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
+    release = models.BooleanField(default=False)
 
 
 class Topic(CommonModel):
@@ -100,14 +98,15 @@ class Topic(CommonModel):
         return out
 
     title = models.CharField(max_length=MAX_LENGTH)
-    presentor = models.ForeignKey(Presentor,blank=True,null=True)
-    meeting = models.ForeignKey( Meeting, blank=True, null=True)
-    length = IntervalField(format="M", blank = True, null = True)
-    embed_video = models.TextField(blank=True,null=True)
-    description = models.TextField(blank=True,null=True)
+    presentor = models.ForeignKey(Presentor, blank=True, null=True)
+    meeting = models.ForeignKey(Meeting, blank=True, null=True)
+    length = IntervalField(format="M", blank=True, null=True)
+    embed_video = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     slides_link = models.URLField(verify_exists=True, blank=True, null=True)
-    start_time = models.DateTimeField(blank = True, null = True)
-    approved = models.BooleanField(default = False)
+    start_time = models.DateTimeField(blank=True, null=True)
+    approved = models.BooleanField(default=False)
+
 
 class RSVP(CommonModel):
     RSVP_CHOICES = (
@@ -116,12 +115,12 @@ class RSVP(CommonModel):
         ('M', "Maybe"),
     )
 
-    user = models.ForeignKey(User, blank = True, null = True)
-    name = models.CharField(max_length = MAX_LENGTH, blank = True, null = True)
-    email = models.EmailField(max_length = 255, blank = True, null = True)
+    user = models.ForeignKey(User, blank=True, null=True)
+    name = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
     meeting = models.ForeignKey(Meeting)
-    response = models.CharField(max_length = 1, choices = RSVP_CHOICES)
-    key = models.CharField(max_length = MAX_LENGTH, blank = True, null = True)
+    response = models.CharField(max_length=1, choices=RSVP_CHOICES)
+    key = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
 
     def clean(self):
         from django.core.exceptions import ValidationError
@@ -132,10 +131,10 @@ class RSVP(CommonModel):
         # Check uniqueness
         if not self.id:
             if self.user:
-                if RSVP.objects.filter(meeting = self.meeting, user = self.user).exists():
+                if RSVP.objects.filter(meeting=self.meeting, user=self.user).exists():
                     raise ValidationError('User has already RSVPed for meeting')
             else:
-                if RSVP.objects.filter(meeting = self.meeting, name = self.name).exists():
+                if RSVP.objects.filter(meeting=self.meeting, name=self.name).exists():
                     raise ValidationError('User has already RSVPed for meeting')
 
     def save(self, *args, **kwargs):
@@ -146,9 +145,9 @@ class RSVP(CommonModel):
         if not self.pk and not self.user:
             self.key = ''.join(random.choice(string.digits + string.ascii_lowercase) for x in range(40))
             plaintext = get_template('meetings/rsvp_email.txt')
-            htmly     = get_template('meetings/rsvp_email.html')
+            htmly = get_template('meetings/rsvp_email.html')
 
-            d = Context({ 'key': self.key, 'site': Site.objects.get_current(),})
+            d = Context({'key': self.key, 'site': Site.objects.get_current()})
 
             subject = 'Chipy: Link to Change your RSVP'
             from_email = 'DoNotReply@chipy.org'
@@ -157,8 +156,6 @@ class RSVP(CommonModel):
             msg = EmailMultiAlternatives(subject, text_content, from_email, [self.email])
             msg.attach_alternative(html_content, "text/html")
             msg.send()
-
-
 
         return super(RSVP, self).save(*args, **kwargs)
 

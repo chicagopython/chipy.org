7eb745b8319e8523280e7267afdbf26c09f1f90c
Cezar Jenkins <emperorcezar@gmail.com>
2014-06-03 08:49:59 -0500

    I am bad a python

--- chipy_org/apps/profiles/urls.py
+++ chipy_org/apps/profiles/urls.py
@@ -4,5 +4,5 @@ from .views import ProfilesList, ProfileEdit
 
 urlpatterns = patterns("",
     url(r'^list/$', ProfilesList.as_view(), name='list'),
-    url(r'^edit/$', login_required(ProfileEdit).as_view(), name='edit'),
+    url(r'^edit/$', login_required(ProfileEdit.as_view()), name='edit'),
 )

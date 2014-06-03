f2d34fa3153448ab6a893fba45ae48b52d7759db
Cezar Jenkins <emperorcezar@gmail.com>
2014-06-03 07:49:19 -0500

    Add login required for profile edit

--- chipy_org/apps/profiles/urls.py
+++ chipy_org/apps/profiles/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
-from profiles.views import (ProfilesList,
-                            ProfileEdit,
-)
+from .views import ProfilesList, ProfileEdit
 
 urlpatterns = patterns("",
     url(r'^list/$', ProfilesList.as_view(), name='list'),
-    url(r'^edit/$', ProfileEdit.as_view(), name='edit'),
+    url(r'^edit/$', login_required(ProfileEdit).as_view(), name='edit'),
 )

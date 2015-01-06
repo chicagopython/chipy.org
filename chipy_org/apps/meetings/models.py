e631643be0ea47c292942980fd8e21a00b695cc4
Adam Jenkins <emperorcezar@gmail.com>
2015-01-06 17:08:40 -0600

    Add meetup_id to the meeting model

--- chipy_org/apps/meetings/models.py
+++ chipy_org/apps/meetings/models.py
@@ -65,6 +65,7 @@ class Meeting(CommonModel):
     where = models.ForeignKey(Venue, blank=True, null=True)
     key = models.CharField(max_length=40, unique=True, blank=True)  # Used for anonymous access to meeting information like RSVPs
     live_stream = models.CharField(max_length=500, null=True, blank=True)
+    meetup_id = models.TextField(blank=True, null=True)
 
     def is_future(self):
         return bool(self.when >= (datetime.datetime.now() - datetime.timedelta(hours=3)))

f3a79274ac74732810d867637f1f4290d21e3af7
Joe Jasinski <joe.jasinski@gmail.com>
2015-02-15 10:28:01 -0600

    added the dj_static Cling call back to help serve up static media

--- chipy_org/deploy/wsgi.py
+++ chipy_org/deploy/wsgi.py
@@ -11,4 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chipy_org.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())

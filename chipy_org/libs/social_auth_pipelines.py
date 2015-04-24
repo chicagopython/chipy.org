4bfa6627b14c3e00e32b10a2806f02d4fafd6509
Adam Jenkins <emperorcezar@gmail.com>
2015-04-23 22:28:44 -0500

    Fix false positive for the social login

--- chipy_org/libs/social_auth_pipelines.py
+++ chipy_org/libs/social_auth_pipelines.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext
 from social_auth.exceptions import AuthAlreadyAssociated
 from social_auth.backends.pipeline.associate import associate_by_email as super_associate_by_email
@@ -6,12 +7,17 @@ from social_auth.backends.pipeline.associate import associate_by_email as super_
 def associate_by_email(*args, **kwargs):
     """Check if a user with this email already exists. If they do, don't create an account."""
     backend = kwargs['backend']
-    if backend.name == 'google-oauth2':
+    if backend.name in ['google-oauth2', 'github']:
         # We provide and exception here for users upgrading.
         return super_associate_by_email(*args, **kwargs)
 
-    msg = ugettext('This email is already in use. First login with your other account and '
-                   'under the top right menu click add account.')
-    raise AuthAlreadyAssociated(backend, msg % {
-        'provider': backend.name
-    })
+    email = kwargs['details'].get('email')
+
+    if email:
+        User = get_user_model()
+        if User.objects.filter(email=email).exists():
+            msg = ugettext('This email is already in use. First login with your other account and '
+                           'under the top right menu click add account.')
+            raise AuthAlreadyAssociated(backend, msg % {
+                'provider': backend.name
+            })

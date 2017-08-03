a10c60803f416c4432b9b1aea1cd0fde15e28066
Joe Jasinski <joe.jasinski@gmail.com>
2017-08-02 20:54:16 -0500

    fix relative import

--- chipy_org/apps/profiles/admin.py
+++ chipy_org/apps/profiles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from models import UserProfile
+from .models import UserProfile
 
 
 class UserProfileInline(admin.StackedInline):

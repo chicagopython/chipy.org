93c8e0fded8c150aaf925843a9e4f9a4b76166cc
raymondberg <dev@raymondberg.com>
2019-07-01 22:17:47 -0500

    Improve ToS Agreement on Topic submission
    Right now the ToS is not visible or clickable on smaller devices. The
    width is too large, and the lack of a max height prevents the user from
    scrolling to see the whole modal.
    
    This PR is a hacky improvement to just ensure that one is able to scroll
    down if the text isn't visable. It also dictates that modals should be a
    percentage of the total, not a fixed width size.

--- chipy_org/apps/meetings/templates/meetings/_tos_dialog.js
+++ chipy_org/apps/meetings/templates/meetings/_tos_dialog.js
@@ -1,6 +1,6 @@
 $('#propose_submit').click(function(event){
     event.preventDefault();
-    $('#tos-dialog').dialog({width: '500px'});
+    $('#tos-dialog').dialog({width: '50%', modal: true, height: 600});
     $('#agree').click(function(){
         $('#propose-topic').submit();
     });

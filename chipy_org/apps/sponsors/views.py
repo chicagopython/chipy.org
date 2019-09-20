0c2f03e127d9f99b8ae5678b012e0720b6974cdf
Sameet Mahisekar <sameetmahisekar@Sameets-MacBook-Pro.local>
2019-09-20 14:40:10 -0500

    commited to

--- chipy_org/apps/sponsors/views.py
+++ chipy_org/apps/sponsors/views.py
@@ -1,6 +1,9 @@
+import datetime
+from datetime import date
+
 from django.views.generic import DetailView, ListView
 from .models import Sponsor
-
+from ..meetings.models import Meeting
 
 class SponsorDetailView(DetailView):
     model = Sponsor
@@ -9,9 +12,20 @@ class SponsorDetailView(DetailView):
 
 
 class SponsorListView(ListView):
-    model = Sponsor
-    context_object_name = "sponsors"
-    template_name = "sponsors/sponsor_list.html"
+	context_object_name = "sponsors"
+	template_name = "sponsors/sponsor_list.html"
+   	queryset = Sponsor.objects.all()
 
-    def get_queryset(self):
-        return super(SponsorListView, self).get_queryset().order_by('name')
+	def get_context_data(self, *args, **kwargs):
+		sponsor_list = Sponsor.objects.all().order_by('name')
+		meeting_queryset = Meeting.objects.all().filter(when__range=((date.today() - datetime.timedelta(days=365),date.today())))
+		meeting_attendees = 0
+		for meeting in meeting_queryset:
+			meeting_attendees = meeting_attendees + int(meeting.number_rsvps())
+
+		context = {
+	    	'sponsors' : sponsor_list,
+	    	'meeting_attendees' : meeting_attendees,
+	    }
+
+		return context

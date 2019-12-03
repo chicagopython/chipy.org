0d79b92129942ea34adab9e0e31b28d93e2e47a4
raymondberg <dev@raymondberg.com>
2019-12-02 18:49:31 -0600

    Remove custom pipeline for auth and bump up get_username

--- chipy_org/settings.py
+++ chipy_org/settings.py
@@ -187,10 +187,9 @@ SOCIAL_AUTH_ENABLED_BACKENDS = (
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
-    'chipy_org.libs.social_auth_pipelines.associate_by_email',
+    'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',

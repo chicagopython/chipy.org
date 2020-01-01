c03d8c4491ce224c2458244a98aa17809a9a2571
LeakedMemory <elmq0022@dumn.edu>
2019-12-31 19:14:12 -0600

    add migration logic to copy length to length_minutes

--- chipy_org/apps/meetings/migrations/0010_auto_20191231_1757.py
+++ chipy_org/apps/meetings/migrations/0010_auto_20191231_1757.py
@@ -4,6 +4,17 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
+def populate_length_minutes(apps, schema_editor):
+    Topic = apps.get_model('meetings', 'Topic')
+    for topic in Topic.objects.all():
+        try:
+            minutes = int(topic.length.total_seconds()/60)
+            topic.length_minutes = minutes
+            topic.save()
+        except:
+            pass
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,4 +22,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(populate_length_minutes),
     ]

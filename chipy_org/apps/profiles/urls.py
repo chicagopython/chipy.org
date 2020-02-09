26049f97b6b0437ecdd935be6968427c86fb7aff
LeakedMemory <elmq0022@dumn.edu>
2020-02-09 14:49:16 -0600

    fix test deprecation warnings for include

--- chipy_org/apps/profiles/urls.py
+++ chipy_org/apps/profiles/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from .views import ProfilesList, ProfileEdit
 
 
+app_name = 'profiles'
+
 urlpatterns = [
     url(r'^list/$', ProfilesList.as_view(), name='list'),
     url(r'^edit/$', login_required(ProfileEdit.as_view()), name='edit'),

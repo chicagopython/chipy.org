92cb149724e83057e8a7ec51ed886969997db1c9
LeakedMemory <elmq0022@dumn.edu>
2020-02-09 13:40:44 -0600

    remove deprecated use of context from email.py

--- chipy_org/apps/meetings/email.py
+++ chipy_org/apps/meetings/email.py
@@ -2,7 +2,6 @@ import logging
 from django.contrib.sites.models import Site
 from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
-from django.template import Context
 from django.conf import settings
 
 
@@ -13,10 +12,7 @@ def send_rsvp_email(rsvp):
     try:
         plaintext = get_template('meetings/emails/rsvp_email.txt')
         htmly = get_template('meetings/emails/rsvp_email.html')
-
-        context = Context(
-            {'key': rsvp.key, 'site': Site.objects.get_current()})
-
+        context = {'key': rsvp.key, 'site': Site.objects.get_current()}
         subject = 'Chipy: Link to Change your RSVP'
         from_email = 'DoNotReply@chipy.org'
         text_content = plaintext.render(context)
@@ -34,9 +30,7 @@ def send_meeting_topic_submitted_email(topic):  # pylint: disable=invalid-name
         plaintext = get_template('meetings/emails/meeting_topic_submitted.txt')
         htmly = get_template('meetings/emails/meeting_topic_submitted.html')
         recipients = getattr(settings, "CHIPY_TOPIC_SUBMIT_EMAILS", [])
-        context = Context(
-            {'topic': topic, 'site': Site.objects.get_current()})
-
+        context = {'topic': topic, 'site': Site.objects.get_current()}
         subject = 'Chipy: New Meeting Topic Submitted'
         from_email = 'DoNotReply@chipy.org'
         text_content = plaintext.render(context)

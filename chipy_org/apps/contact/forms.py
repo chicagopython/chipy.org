8ccddd431417e446f5954bf78fa9c907e4702432
LeakedMemory <elmq0022@umn.edu>
2020-05-26 08:12:21 -0500

    adds email functionality

--- chipy_org/apps/contact/forms.py
+++ chipy_org/apps/contact/forms.py
@@ -1,5 +1,12 @@
+import logging
+
 from django import forms
+from django.conf import settings
+from django.core.mail import EmailMessage
 from nocaptcha_recaptcha.fields import NoReCaptchaField
+
+logger = logging.getLogger(__name__)
+
 
 class ContactForm(forms.Form):
     sender = forms.CharField(label="From")
@@ -9,4 +16,16 @@ class ContactForm(forms.Form):
     captcha = NoReCaptchaField()
 
     def send_email(self):
-        pass
+        try:
+            msg = EmailMessage(
+                subject=self.subject,
+                body=self.message,
+                from_email=self.email,
+                to=getattr(settings, "CHIPY_TOPIC_SUBMIT_EMAILS", []),
+                reply_to=[self.email],
+            )
+            msg.send()
+            return True
+        except Exception as e:
+            logger.exception(e)
+            return False
--- chipy_org/apps/contact/views.py
+++ chipy_org/apps/contact/views.py
@@ -7,9 +7,12 @@ from chipy_org.apps.contact.forms import ContactForm
 class ContactView(FormView):
     template_name = "contact/contact.html"
     form_class = ContactForm
-    success_url = "/home/"
+    success_url = "/"
 
     def form_valid(self, form):
-        form.send_email()
-        messages.success(self.request, "Your message has been sent to Chipy's Organizers")
+        success = form.send_email()
+        if success:
+            messages.success(self.request, "Your message has been sent to Chipy's organizers")
+        else:
+            messages.error(self.request, "Your message was NOT sent to Chipy's organizers")
         return super().form_valid(form)

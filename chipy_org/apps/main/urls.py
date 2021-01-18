93836bcaeeec0b0d1956d47e64f35ff5b5332a4a
Sophan Mien <53249319+sophm256@users.noreply.github.com>
2021-01-18 13:31:45 -0800

    Initial site redesign

--- chipy_org/apps/main/urls.py
+++ chipy_org/apps/main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import Home
+from .views import Home, PreviewHome
 
 urlpatterns = [
     url(r"^$", Home.as_view(), name="home"),
+    url(r"^preview/?$", PreviewHome.as_view(), name="home_preview"),
 ]

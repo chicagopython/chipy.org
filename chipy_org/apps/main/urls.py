a19d48fc5257e46602c6115959207d226ddefcdb
raymondberg and sophm256 <dev@raymondberg.com>
2021-01-22 21:27:29 +0000

    Remove old chipy styling html/js/css

--- chipy_org/apps/main/urls.py
+++ chipy_org/apps/main/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 
-from .views import Home, PreviewHome
+from .views import Home
 
 urlpatterns = [
     url(r"^$", Home.as_view(), name="home"),
-    url(r"^preview/?$", PreviewHome.as_view(), name="home_preview"),
 ]

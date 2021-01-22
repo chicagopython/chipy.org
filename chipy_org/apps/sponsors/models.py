8ad48a44d9c0cd1c7d0eb9a96d952ca9affad035
raymondberg and sophm256 <dev@raymondberg.com>
2021-01-22 01:22:21 +0000

    Add featured sponsor to homepage

--- chipy_org/apps/sponsors/models.py
+++ chipy_org/apps/sponsors/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import time
 
 from django.db import models
 from django.urls import reverse
@@ -26,6 +27,7 @@ class MeetingSponsor(models.Model):
 class SponsorGroup(models.Model):
     name = models.CharField(max_length=80)
     list_priority = models.IntegerField(default=5)
+    featured_sponsor_weight = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
@@ -56,8 +58,35 @@ class Sponsor(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    @property
+    def featured_sponsor_weight(self):
+        return self.sponsor_group.featured_sponsor_weight
+
     def get_absolute_url(self):
         return reverse("sponsor_detail", args=[self.slug])
+
+    @classmethod
+    def featured_sponsor(cls, second_shift_constant=10):
+        """
+        Return a featured sponsor based on the current time, the sponsor levels, and a
+        bit shift constant that determines length of overall cycle
+
+          second_shift_constant is the amount seconds will be bitshifted by when
+          calculating the featured sponsor. If set to 0, the cycle of all sponsors
+          will complete in 100 seconds. If set to 1, it will be 200 seconds, 2 is
+          400 seconds, and 12 is 409600 or 4.75 days.
+        """
+        sponsors = cls.objects.select_related("sponsor_group").all()
+
+        current_time_mod = (int(time.time()) >> second_shift_constant) % 100  # eg 36
+
+        total_number_of_chunks = sum(s.featured_sponsor_weight for s in sponsors)
+        chunk_size = 100 / total_number_of_chunks
+        chunk_end = 0
+        for sponsor in sponsors:
+            chunk_end = chunk_end + sponsor.featured_sponsor_weight * chunk_size
+            if current_time_mod < chunk_end:
+                return sponsor
 
     class Meta:
         ordering = ["name"]

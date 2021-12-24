0f8540ccd18d86ec5f4947fec2a3b0c77cb02246
LeakedMemory <elmq0022@umn.edu>
2021-12-24 08:26:46 -0600

    updates test cases for new captcha

--- chipy_org/apps/contact/tests.py
+++ chipy_org/apps/contact/tests.py
@@ -2,43 +2,28 @@ import pytest
 from django.conf import global_settings
 from django.core import mail
 from django.test import override_settings
-from nocaptcha_recaptcha.fields import NoReCaptchaField
+
+from django.conf import settings
 
 from .forms import ContactForm
 
 
-@pytest.fixture
-def no_recaptcha(monkeypatch):
-    monkeypatch.setenv("NORECAPTCHA_TESTING", "True")
-    yield
-
-
+@pytest.mark.django_db
 @override_settings(STATICFILES_STORAGE=global_settings.STATICFILES_STORAGE)
-def test_clean_captcha(no_recaptcha):  # pylint: disable=redefined-outer-name
-    """
-    This example shows how to override the captach for testing purposes used below. The datadict
-    passed to the form and subsquently the widget must contatin the g-recaptcha-response with a
-    value of passed, and there must ben an environment variable NORECAPTCHA_TESTING set to 'True' to
-    by pass the captcha when the form is cleaned.
-    """
-    field = NoReCaptchaField()
-    value = field.widget.value_from_datadict({"g-recaptcha-response": "PASSED",}, {}, {})
-    field.clean(value)
-
-
-@override_settings(STATICFILES_STORAGE=global_settings.STATICFILES_STORAGE)
-def test_chipy_contact_form(no_recaptcha):  # pylint: disable=redefined-outer-name
+def test_chipy_contact_form():  # pylint: disable=redefined-outer-name
     assert len(mail.outbox) == 0
 
     form_data = {
         "email": "test@test.com",
-        "g-recaptcha-response": "PASSED",
         "message": "test message",
         "sender": "test",
         "subject": "test subject",
+        "captcha_0": "dummy",
+        "captcha_1": "PASSED",
     }
 
     form = ContactForm(form_data)
+
     assert form.is_valid()
     form.send_email()
     assert len(mail.outbox) == 1
@@ -46,17 +31,18 @@ def test_chipy_contact_form(no_recaptcha):  # pylint: disable=redefined-outer-na
 
 @pytest.mark.django_db
 @override_settings(STATICFILES_STORAGE=global_settings.STATICFILES_STORAGE)
-def test_chipy_contact_view(client, no_recaptcha):  # pylint: disable=redefined-outer-name
+def test_chipy_contact_view(client):  # pylint: disable=redefined-outer-name
     assert len(mail.outbox) == 0
 
     response = client.post(
         "/contact/",
         {
             "email": "test@test.com",
-            "g-recaptcha-response": "PASSED",
             "message": "test message",
             "sender": "test",
             "subject": "test subject",
+            "captcha_0": "dummy",
+            "captcha_1": "PASSED",
         },
         follow=True,
     )

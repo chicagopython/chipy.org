1e2a71626d35739c41611ca5b9b90d930f0683cb
raymondberg <dev@raymondberg.com>
2022-03-28 19:41:38 -0500

    Update/fix tests following #468

--- chipy_org/apps/profiles/tests/test_models.py
+++ chipy_org/apps/profiles/tests/test_models.py
@@ -6,12 +6,23 @@ from ..models import UserProfile
 pytestmark = pytest.mark.django_db
 
 
-def test_profile_has_organizer_attribute():
+def test_profile_has_role_attribute():
     user = User.objects.create()
 
-    user.profile.is_organizer = True
+    user.profile.role = UserProfile.Role.ORGANIZER
     user.profile.save()
 
     profile = UserProfile.objects.get(user=user)
-
     assert profile.is_organizer
+    assert not profile.is_board_member
+    assert not profile.is_officer
+
+    profile.role = UserProfile.Role.BOARD
+    assert profile.is_organizer
+    assert profile.is_board_member
+    assert not profile.is_officer
+
+    profile.role = UserProfile.Role.TREASURER
+    assert profile.is_organizer
+    assert profile.is_board_member
+    assert profile.is_officer

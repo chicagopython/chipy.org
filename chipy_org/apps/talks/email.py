cc3ec1b37e19ced78772f94e77094e1d4e86e6d6
raymondberg <dev@raymondberg.com>
2023-06-15 19:57:56 -0500

    Fix merge issue

--- chipy_org/apps/talks/email.py
+++ chipy_org/apps/talks/email.py
@@ -1,23 +1,18 @@
 import logging
 
 from django.contrib.sites.models import Site
-from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
+
+from chipy_org.libs.email import send_email
 
 logger = logging.getLogger(__name__)
 
 
 def send_meeting_topic_submitted_email(topic, recipients):  # pylint: disable=invalid-name
-    try:
-        plaintext = get_template("talks/emails/meeting_topic_submitted.txt")
-        htmly = get_template("talks/emails/meeting_topic_submitted.html")
-        context = {"topic": topic, "site": Site.objects.get_current()}
-        subject = "Chipy: New Meeting Topic Submitted"
-        from_email = "DoNotReply@chipy.org"
-        text_content = plaintext.render(context)
-        html_content = htmly.render(context)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, recipients)
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
-    except Exception as e:
-        logger.exception(e)
+    plaintext = get_template("talks/emails/meeting_topic_submitted.txt")
+    htmly = get_template("talks/emails/meeting_topic_submitted.html")
+    context = {"topic": topic, "site": Site.objects.get_current()}
+    subject = "Chipy: New Meeting Topic Submitted"
+    text_content = plaintext.render(context)
+    html_content = htmly.render(context)
+    send_email(recipients, subject, text_content, html_content, swallow_errors=True)

4ab207a24b842c31845d12a65847b96e9a61dddb
H. White <eevel.weezel@gmail.com>
2025-08-27 20:49:26 -0500

    Fix the contact form again (#603)
    * remove crispy FormHelper
    
    * fix template
    
    * remove unused dep that angers cython
    
    * Fix form formatting

--- chipy_org/apps/contact/forms.py
+++ chipy_org/apps/contact/forms.py
@@ -1,7 +1,5 @@
 import logging
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from django import forms
 from django.conf import settings
 from django_recaptcha.fields import ReCaptchaField
@@ -15,11 +13,9 @@ logger = logging.getLogger(__name__)
 class ContactForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(Submit("submit", "Send Email"))
         self.fields["captcha"].label = False
 
-    sender = forms.CharField(max_length=256, label="From")
+    sender = forms.CharField(max_length=256, label="Name")
     email = forms.EmailField(max_length=256)
     subject = forms.CharField(max_length=256)
     message = forms.CharField(
